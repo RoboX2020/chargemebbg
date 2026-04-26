f537aa5c138cbda9c215285b11ccd76115f816c1
PRAJVAL ARORA <dr.arora@machine.local>
2026-04-25 23:06:45 -0700

    Add new custom explicit sounds with moaning vocal tweaks

---
+++ scripts/generate_custom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="/Users/dr.arora/chargemebbg/sounds/in"
+
+say -v "Victoria" "[[rate 90]] [[pbas 35]] Nauuughtyy,, yoouuu." -o "$DIR/ai_naughty.aiff"
+say -v "Victoria" "[[rate 80]] [[pbas 30]] Fffuucckkk,,." -o "$DIR/ai_fuck.aiff"
+say -v "Victoria" "[[rate 100]] [[pbas 35]] Nooobody,, doeess itt,, beeetteer." -o "$DIR/ai_nobody.aiff"
+
+echo "Custom AI sounds generated!"
